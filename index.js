b9109c84ac0005ef53843332045cccd7af8bf488
Miroslav Popov <info@forexsb.com>
2020-08-30 05:01:55 +0000

    Print in colors.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const colors = {
+    Reset     : '\x1b[0m',
+    Bright    : '\x1b[1m',
+    Dim       : '\x1b[2m',
+    Underscore: '\x1b[4m',
+    Blink     : '\x1b[5m',
+    Reverse   : '\x1b[7m',
+    Hidden    : '\x1b[8m',
+
+    FgBlack   : '\x1b[30m',
+    FgRed     : '\x1b[31m',
+    FgGreen   : '\x1b[32m',
+    FgYellow  : '\x1b[33m',
+    FgBlue    : '\x1b[34m',
+    FgMagenta : '\x1b[35m',
+    FgCyan    : '\x1b[36m',
+    FgWhite   : '\x1b[37m',
+
+    BgBlack   : '\x1b[40m',
+    BgRed     : '\x1b[41m',
+    BgGreen   : '\x1b[42m',
+    BgYellow  : '\x1b[43m',
+    BgBlue    : '\x1b[44m',
+    BgMagenta : '\x1b[45m',
+    BgCyan    : '\x1b[46m',
+    BgWhite   : '\x1b[47m',
+}
+
 /**
  * @typedef { object } TestStats
  *
@@ -37,9 +65,11 @@ function getNewStats() {
 function describe(message, func) {
     if (stats.level === 0) {
         console.log();
+        log(colors.Bright + message + colors.Reset);
     }
-
-    log(message);
+    else {
+        log(colors.Dim + message + colors.Reset);
+    }
 
     stats.level++;
 
@@ -70,12 +100,12 @@ function it(message, func) {
     try {
         func();
 
-        log(stats.index + ". ✅ " + message);
+        logSuccess(stats.index + ". ✅ " + message);
         stats.passed++;
     } catch (e) {
-        error(stats.index + ". ❌ " + message);
-        error(e.message);
-        error("Actual: " + e.actual + ", Expected: " + e.expected);
+        logError(stats.index + ". ❌ " + message);
+        logError(e.message);
+        logError("Actual: " + e.actual + ", Expected: " + e.expected);
         stats.failed++;
     }
 }
@@ -84,16 +114,18 @@ function it(message, func) {
  * Shows a test summary. It resets the stats.
  */
 function showStats() {
-    const message = `Passed: ${stats.passed} of ${stats.index}, Failed: ${stats.failed}`;
-    const failed = stats.failed;
+    const passedText = stats.passed === stats.index
+        ? colors.FgGreen + 'Passed: ' + stats.passed + ' of ' + stats.index + colors.Reset
+        : 'Passed: ' + stats.passed + ' of ' + stats.index;
 
+    const failedText = stats.failed
+        ? colors.FgRed + 'Failed: ' + stats.failed + colors.Reset
+        : 'Failed: ' + stats.failed;
+
+    const failed = stats.failed;
     stats = getNewStats();
 
-    if (failed) {
-        error(message);
-    } else {
-        log(message);
-    }
+    log(passedText + ', ' + failedText);
 
     if (failed) {
         throw new Error("Tests failed: " + failed);
@@ -110,12 +142,21 @@ function log(message) {
 }
 
 /**
- * Logs an error message to the console with a proper indentation
+ * Logs a successful test
+ *
+ * @param { string } message - success message
+ */
+function logSuccess(message) {
+    console.log(indentation() + colors.FgGreen + message + colors.Reset);
+}
+
+/**
+ * Logs a failed test
  *
  * @param { string } message - error message
  */
-function error(message) {
-    console.error(indentation() + message);
+function logError(message) {
+    console.log(indentation() + colors.FgRed + message + colors.Reset);
 }
 
 /**
